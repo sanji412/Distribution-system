d9caaa5a4246556dbf78cbc525d0ba18c41af639
Thtrace <1247487792@qq.com>
2026-06-25 17:55:49 +0800

    Initial project setup

---
+++ day1-monolith/src/main/java/com/buu/distribution/Day1MonolithApplication.java
@@ -0,0 +1,13 @@
+package com.buu.distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1MonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day1MonolithApplication.class, args);
+    }
+
+}
---
+++ day1-monolith/src/test/java/com/buu/distribution/Day1MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buu.distribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1MonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
